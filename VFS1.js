a7b2537c46862a9b68feea78fae3a2be329d777a
geographyJSprofessor <131638714+geographyJSprofessor@users.noreply.github.com>
2023-04-24 01:02:11 -0400

    Add files via upload

---
+++ VFS1.js
@@ -0,0 +1,192 @@
+var a = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "2023 10th Biennial Autonomous VTOL Technical Meeting and 10th Electric VTOL Symposium",
+        "Location": "Mesa, Arizona",
+        "Date": "January 24–26, 2023",
+        "Website": "http://www.vtol.org/autonomous"
+      },
+      "geometry": {
+        "coordinates": [
+          -111.8320477754347,
+          33.414820267014775
+        ],
+        "type": "Point"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "2023 Handling Qualities Technical Meeting",
+        "Location": "Huntsville, Alabama",
+        "Date": "February 7–8, 2023",
+        "Website": "http://www.vtol.org/handling"
+      },
+      "geometry": {
+        "coordinates": [
+          -86.58575625814589,
+          34.728349560709816
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Heli-Expo 2023",
+        "Location": "Atlanta, Georgia",
+        "Date": "March 6–9, 2023",
+        "Website": "http://www.heliexpo.com"
+      },
+      "geometry": {
+        "coordinates": [
+          -84.39100783045674,
+          33.74685020611905
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "2023 2nd H2-Aero Symposium",
+        "Location": "Long Beach, California",
+        "Date": "March 28–30, 2023",
+        "Website": "http://www.vtol.org/h2symposium"
+      },
+      "geometry": {
+        "coordinates": [
+          -118.1909622358503,
+          33.76806732393311
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Naval Helicopter Association Symposium",
+        "Location": "Harrah's Resort Southern California",
+        "Date": "May 15–19, 2023",
+        "Website": "http://www.navalhelicopterassn.org/symposium"
+      },
+      "geometry": {
+        "coordinates": [
+          -117.03349796056719,
+          33.219970211852754
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": " 79th Annual Forum & Technology Display - \"The Future of Vertical Flight\"",
+        "Location": "West Palm Beach, Florida",
+        "Date": "May 16-18, 2023",
+        "Website": "http://www.vtol.org/forum"
+      },
+      "geometry": {
+        "coordinates": [
+          -80.05221203721244,
+          26.712985833753905
+        ],
+        "type": "Point"
+      },
+      "id": 5
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Airborne Public Safety Association Conference 2023 (APSCON)",
+        "Location": "Orlando, Florida",
+        "Date": "July 17–22, 2023",
+        "Website": "http://www.publicsafetyaviation.org/apscom"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.4692787288353,
+          28.42527915303083
+        ],
+        "type": "Point"
+      },
+      "id": 6
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "International Powered Lift Conference (IPLC) 2023",
+        "Location": "San Diego, California",
+        "Date": "June 12–16, 2023",
+        "Website": "http://www.vtol.org/iplc"
+      },
+      "geometry": {
+        "coordinates": [
+          -117.16288334116993,
+          32.71634598442073
+        ],
+        "type": "Point"
+      },
+      "id": 7
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "2023 17th Annual Electric Aircraft Symposium",
+        "Location": "Oshkosh, Wisconsin",
+        "Date": "July 22–23, 2023",
+        "Website": "http://www.vtol.org/eas"
+      },
+      "geometry": {
+        "coordinates": [
+          -88.54588466189485,
+          44.020906628323615
+        ],
+        "type": "Point"
+      },
+      "id": 8
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "2023 Air Medical Transport Conference",
+        "Location": "Columbus, Ohio",
+        "Date": "October 23–25, 2023",
+        "Website": "http://www.aams.org/page/AMTC23"
+      },
+      "geometry": {
+        "coordinates": [
+          -83.0022373173086,
+          39.96232238010796
+        ],
+        "type": "Point"
+      },
+      "id": 9
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Helicopter Association International Aerial Work Safety Conference",
+        "Location": "Boise, Idaho",
+        "Date": "November 16–17, 2023",
+        "Website": "http://www.rotor.org/aerialworksafetyconf"
+      },
+      "geometry": {
+        "coordinates": [
+          -116.20389452399417,
+          43.615145393688096
+        ],
+        "type": "Point"
+      },
+      "id": 10
+    }
+  ]
+}
